d61de4b68fb876912eb582633826552993d2532a
Anas KBL <kabila.anas1@gmail.com>
2024-01-20 15:44:43 +0100

    added api connection

--- app/src/main/java/com/example/applemusic/model/Track.kt
+++
@@ -1,8 +0,0 @@
-package com.example.applemusic.model
-
-data class Track(
-    val name: String,
-    val imageUrl: String,
-    val artistName: String,
-    val category: String
-)
--- app/src/main/java/com/example/applemusic/service/ITunesApiService.kt
+++ app/src/main/java/com/example/applemusic/service/ITunesApiService.kt
@@ -1,22 +1,2 @@
 package com.example.applemusic.service
 
-import com.example.applemusic.model.ApiResponse
-import okhttp3.Response
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ITunesApiService {
-    @GET("rss/topsongs/limit=100/json")
-    fun getTracks(): Call<ApiResponse>
-
-    // create an instance of the ITunesApiService interface
-    companion object Factory {
-        fun create(): ITunesApiService {
-            val retrofit = retrofit2.Retrofit.Builder()
-                .baseUrl("https://itunes.apple.com/")
-                .build()
-
-            return retrofit.create(ITunesApiService::class.java)
-        }
-    }
-}
